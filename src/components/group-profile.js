9b276ac22c25113e0ec8fec4bad9feb866952736
Kaitlin Bunn <kabunn1991@gmail.com>
2018-08-16 16:53:02 -0400

    all styling done

--- src/components/group-profile.js
+++ src/components/group-profile.js
@@ -3,6 +3,8 @@ import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 import { fetchOneGroup } from '../actions/groups';
 import './group-profile.css';
+import NavBar from './nav-bar';
+import { fetchOneUser } from '../actions/users';
 
 export class  GroupProfile extends React.Component {
 
@@ -10,30 +12,44 @@ export class  GroupProfile extends React.Component {
     this.props.dispatch(fetchOneGroup(this.props.match.params.id));  
   }
 
+  userGroup() {
+    console.log(this.props.currentGroup.users);
+    if (this.props.currentGroup !== null) {
+      if (this.props.currentGroup.users !== undefined) {
+        return this.props.currentGroup.users.map((user, i) => (
+          <li key={i}> <div className="userImage"><img src={user.profileImage}></img></div> <div className="userInfo"><a href={`/users/${user._id}`} className="userName">{user.username}</a></div>
+          
+          <div className="biotext">{user.bio}</div> </li>
+        ))
+      }
+    } else {
+      return <h3>loading</h3>
+    }
+  }
+
   render() {
-    // console.log(this.props.currentGroup);
+
     if (this.props.currentGroup) {
-
-
-    console.log(this.props.currentGroup.game);
 
     let bgImg = (this.props.currentGroup.game === "Bloodborne") ? "topContBloodborne" : (this.props.currentGroup.game === "Overwatch") ? "topContDva" : (this.props.currentGroup.game === "Bioshock") ? "topContBioshock" : (this.props.currentGroup.game === "Battlefront") ? "topContBattlefront" : ""
 
-    console.log(bgImg);
+    // console.log(bgImg);
 
       let groupName = this.props.currentGroup.groupName;
       // let games = this.props.users.games.map(game => <li>-{game}</li>);
       let game = this.props.currentGroup.game;
-      console.log(game);
+      // console.log(game);
       // let gameList = games ? games.map((game,i) => <li key={i}>{game}</li>) : [];
 
       return (
-        <div>
+        <div className="groupProf">
+          <NavBar />
           <div className={bgImg}>
+            <div className="groupName">{groupName}</div>
+            <div className="game">A {game} group</div>
           </div>
-          {groupName}'s Profile!!!!
-          <div><img src="http://placehold.it/100x100" /></div>
-          Game: <ul>{game}</ul>
+          <ul>{this.userGroup()}</ul>
+          
         </div>
       )
     } else {
@@ -47,7 +63,7 @@ const mapStateToProps = state => {
   return {
       protectedData: state.protectedData.data,
       currentGroup: state.groupReduce.currentGroup,
-      userId: state.auth.currentUser ? state.auth.currentUser._id : null
+      userId: state.auth.currentUser ? state.auth.currentUser._id : null,
   };
 };
 
